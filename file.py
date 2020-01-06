cae2d41f3485165cc5bce992f62ddde0a2c29b2b
minkyeong417 <55428050+minkyeong417@users.noreply.github.com>
2020-01-06 20:50:37 +0900

    file

--- file.py
+++ file.py
@@ -1,5 +1,10 @@
 filename = input('Enter a file name: ')
 handle = open(filename,'r')
-str = handle.read()
-str = str.rstrip()
-print(str.upper())
+sumb = 0
+count = 0
+for str in handle:
+    if str.startswith('X-DSPAM-Confidence:'):
+        count = count + 1
+        index = str.find('0')
+        sumb = sumb + float(str[index:])
+print('Average spam confidence:',sumb/count)
